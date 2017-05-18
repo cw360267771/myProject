0352a26386a8d11c810885fbfa28db8af21a3578
cw360267771 <360267771@qq.com>
2017-05-18 18:25:17 +0800

    必须提交本地库才能上传到github

--- test/src/com/git/project/Test.java
+++ test/src/com/git/project/Test.java
@@ -8,7 +8,7 @@ public class Test {
 	public static void main(String[] args) {
 	System.out.println("sss");
 	String abc ="";
-	String chenwei="wo shi cesi de";
+	String chenwei="wo shi cesi de 多少度";
 
 	}
 
